6f231c699e3f55aa74e5cdf2781c33ff6ca45daa
Divya5595 <divya5595@gmail.com>
2022-02-26 14:08:37 -0500

    Update correlation.py
    Added comments and updated dataset

--- correlation.py
+++ correlation.py
@@ -3,11 +3,14 @@ import pandas as pd
 import seaborn as sn
 import pylab as pl
 
-data = pd.read_csv('reviews_NewYork.csv')
-df = pd.DataFrame(data)
-print(data.head)
+# reading the data from csv into a dataframe
+df = pd.read_csv ('wic.csv')
+
+# using the inbuild library - corr() to create a correlation matrix 
+# this will help study the importance of each feature compared to the other
 corrMatrix = df.corr()
-print(corrMatrix)
+
+# plot the heat map obtained 
 plt.figure(figsize=(8,8))
 sn.heatmap(corrMatrix, annot=True)
 plt.title("Heatmap for India Combined Data")
